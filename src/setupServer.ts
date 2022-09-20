c3fa44edf47b73320c044188336557f0b3f3f77c
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-09-20 17:42:56 +0900

    update

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -8,6 +8,8 @@ import HTTP_STATUS from 'http-status-codes'
 import 'express-async-errors'
 import helmet from 'helmet'
 
+const SERVER_PORT = 5000
+
 export class ChattyServer {
   private app: Application
 
@@ -55,9 +57,20 @@ export class ChattyServer {
 
   private globalErrorHandler(app: Application): void {}
 
-  private startServer(app: Application): void {}
+  private async startServer(app: Application): Promise<void> {
+    try {
+      const httpServer: http.Server = new http.Server(app)
+      this.startHttpServer(httpServer)
+    } catch (error) {
+      console.info(error)
+    }
+  }
 
   private createSocketIO(httpServer: http.Server): void {}
 
-  private startHttpServer(httpServer: http.Server): void {}
+  private startHttpServer(httpServer: http.Server): void {
+    httpServer.listen(SERVER_PORT, () => {
+      console.info(`Server running on port ${SERVER_PORT}`)
+    })
+  }
 }
